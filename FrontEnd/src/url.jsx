560199635d30e276b304ae8c360a6db36ac258b5
MaherMarawi <mahermarawi92@gmail.com>
2022-10-30 18:16:15 +0100

    update

--- FrontEnd/src/url.jsx
+++ FrontEnd/src/url.jsx
@@ -1,3 +1,3 @@
 // const url = process.env.NODE_ENV === 'development' ? 'https://portfolio-backend-c3k1.onrender.com/api': '/api'
-const url = 'https://portfolio-backend-c3k1.onrender.com/api'
+const url = 'https://portfolio-backend-c3k1.onrender.com'
 export default url
