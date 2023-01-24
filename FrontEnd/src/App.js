868845de1bf6d30378b779d52283247e8a73e676
MaherMarawi <mahermarawi92@gmail.com>
2023-01-24 11:06:29 +0100

    update font

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
-import 'semantic-ui-css/semantic.min.css'
+// import 'semantic-ui-css/semantic.min.css'
 import 'rebass'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Components/Home'
