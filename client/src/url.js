11d5c195f5b00469842e65f8a48771de51e5fdfb
MaherMarawi <mahermarawi92@gmail.com>
2023-02-16 12:46:29 +0100

    admin update

--- client/src/url.js
+++ client/src/url.js
@@ -1,3 +1,3 @@
-const url = process.env.NODE_ENV === 'development' ? 'https://portfolio-backend-c3k1.onrender.com/api': 'http://localhost:8000/api'
+const url = 'https://portfolio-backend-c3k1.onrender.com/api'
 
 export default url
